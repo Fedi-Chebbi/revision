696419f8c4d21a3cdbd35277755c59c06d7dd89c
Fedi-Chebbi <fedi.chebbi01@outlook.fr>
2023-12-22 16:30:27 +0100

    done

--- C-F-V.js
+++ C-F-V.js
@@ -12,18 +12,18 @@ if (a>b){
 return a} 
 return b}
 function add(number1, number2){
-number1+number2
+return number1+number2
 }
 function subtract(number1, number2){
-    number1-number2 
+return number1-number2 
 }
 var operator = '+'
 var p = 5
 var m = 3
-function decide(p,m)
-if (operator){
-return add}
-else return subtract 
+function decide(){
+if (operator==='+'){
+return add(p,m)}
+else return subtract(p,m) }
 function divide(a,b){
     a/b
 } 
