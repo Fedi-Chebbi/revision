883ff0d7eac71355adc90accb8b7f7cf811b1425
Fedi-Chebbi <fedi.chebbi01@outlook.fr>
2023-12-22 16:14:52 +0100

    done

---
+++ C-F-V.js
@@ -0,0 +1,32 @@
+var carName = "Volvo"
+
+function square(x){
+    return x*x
+}
+// this is the coreect statement of  square//
+// the first one mistake is that you should put x into parenthese//
+// the second one mistake is there is one missing parenthese//
+// the third one mistake is we should put the parametre before the function name//
+function largest(a,b){
+if (a>b){    
+return a} 
+return b}
+function add(number1, number2){
+number1+number2
+}
+function subtract(number1, number2){
+    number1-number2 
+}
+var operator = '+'
+var p = 5
+var m = 3
+function decide(p,m)
+if (operator){
+return add}
+else return subtract 
+function divide(a,b){
+    a/b
+} 
+function multiply(a,b){
+    a*b
+} 
